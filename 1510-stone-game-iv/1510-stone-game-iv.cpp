913764a106ec86a6f905b0597597ddb9a2fb839c
Hariharan <hariharan170000@gmail.com>
2026-08-10 18:33:54 +0530

    Time: 80 ms (29.23%), Space: 7.9 MB (97.28%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool winnerSquareGame(int n) {
+        bool dp[n+1];
+        dp[0] = false;
+        for (int i = 1; i <= n; i++) {
+            dp[i] = false;
+            for (int j = 1; j * j <= i; j++) {
+                if (dp[i - j * j] == false) {
+                    dp[i] = true;
+                }
+            }
+        }
+        return dp[n];
+    }
+};
